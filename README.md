eaef72ab8556ebdbd840afb4ff96034f98cebee4
Varun Mithyantha <varun.mithyantha@gmail.com>
2023-06-18 11:56:04 +0530

    Styling with Classes

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
-# co38a4834950b81f65b4187b7
+# co9a8472da55bf26d30d348ba
 
 Quick start:
 
 ```
-$ npm install
-$ npm start
+$ yarn # npm install
+$ yarn build # npm run build
 ````
 
-Head over to https://vitejs.dev/ to learn more about using vite
+## Development
+
+Run Webpack in watch-mode to continually compile the JavaScript as you work:
+
+```
+$ yarn watch # npm run watch
+```
+
 ## About Scrimba
 
 At Scrimba our goal is to create the best possible coding school at the cost of a gym membership! 💜
--- index.js
+++ index.js
@@ -1,68 +1,62 @@
-// function Navbar() {
-//     return (
-//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//             <a className="navbar-brand" href="#">Navbar</a>
-//             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//                 <span className="navbar-toggler-icon"></span>
-//             </button>
+import React from "react"
+import ReactDOM from "react-dom"
 
-//             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//                 <ul className="navbar-nav mr-auto">
-//                 <li className="nav-item active">
-//                     <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-//                 </li>
-//                 <li className="nav-item">
-//                     <a className="nav-link" href="#">Link</a>
-//                 </li>
-//                 <li className="nav-item dropdown">
-//                     <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-//                     Dropdown
-//                     </a>
-//                     <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-//                     <a className="dropdown-item" href="#">Action</a>
-//                     <a className="dropdown-item" href="#">Another action</a>
-//                     <div className="dropdown-divider"></div>
-//                     <a className="dropdown-item" href="#">Something else here</a>
-//                     </div>
-//                 </li>
-//                 <li className="nav-item">
-//                     <a className="nav-link disabled" href="#">Disabled</a>
-//                 </li>
-//                 </ul>
-//                 <form className="form-inline my-2 my-lg-0">
-//                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-//                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-//                 </form>
-//             </div>
-//         </nav>
-//     )
-// }
+/**
+Challenge: 
 
-// function MainContent(){
-//     return (
-//         <h1>I am learning React</h1>
-//     )
-// }
-// ReactDOM.render(
-//     <div>
-//             <Navbar />,
-//             <MainContent/>
-//     </div>,
+- Add an `ul` inside the Header's `nav` and create
+  the following `li`s: "Pricing", "About", & "Contact"
+- Using flexbox, line up the nav items horizontally, and
+  put them inline with the React logo.
+- Change the image styling to happen in CSS instead of in-line
+  For practice, add a new class to the image in order to style it
+*/
 
-//     document.getElementById("root")
-// )
+function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img src="./react-logo.png" className="nav-logo" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
 
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
 
+function MainContent() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+        </div>
+    )
+}
 
-const navbar = (
-    <nav>
-        <h1>Bob's Bistro</h1>
-        <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
-)
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
 
-ReactDOM.render(navbar, document.getElementById("root"))
+ReactDOM.render(<Page />, document.getElementById("root"))
